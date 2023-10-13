e220446e4f06c6c3b340c1bc66e954bf34d7eff5
kriti <kritijulia@gmail.com>
2023-10-14 01:40:24 +0530

    Insert at the front of linked list

--- DSA_main/Linked_list/InsertAtFront.cpp
+++ DSA_main/Linked_list/InsertAtFront.cpp
@@ -5,11 +5,25 @@ class Node{
     int data;
     Node* next;
 };
-void InsertAtFront(Node **head_ref ,int data){
-    Node* new_node = new Node();
+// method 1
+// void InsertAtFront(Node **head_ref ,int data){
+//     Node* new_node = new Node();
+//     new_node->data = data;
+//     new_node->next = (*head_ref);
+//     (*head_ref) = new_node;
+// }
+//method 2
+void insert_beg(Node* &head,int data){
+    Node* new_node =new Node();
     new_node->data = data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
+    if(head==NULL){
+        head=new_node;
+        return;
+    }
+    
+    new_node->next = head;
+    head = new_node;
+    return ;
 }
 void Display(Node* node){
     while(node!=NULL){
@@ -19,13 +33,22 @@ void Display(Node* node){
     cout<<"\n";
 }
 int main(){
-    Node* head = NULL;
-    InsertAtFront(&head,1);
-    InsertAtFront(&head,2);
-    InsertAtFront(&head,3);
-    InsertAtFront(&head,4);
-    InsertAtFront(&head,5);
-    InsertAtFront(&head,6);
+    int n;
+    cout<<"how many data you want to enter: ";cin>>n;
+    int data;
+
+    Node* head =NULL;
+    // InsertAtFront(&head,1);
+    // InsertAtFront(&head,2);
+    // InsertAtFront(&head,3);
+    // InsertAtFront(&head,4);
+    // InsertAtFront(&head,5);
+    // InsertAtFront(&head,6);
+    cout<<"insert the data: ";
+    for(int i=0; i<n;i++){
+        cin>>data;
+        insert_beg(head,data);
+    }
     cout << "After inserting Nodes at their front: ";
     Display(head);
     return 0;
