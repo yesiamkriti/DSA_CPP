f51912cc8fdba6668e2fc6615c873a31e4c4828b
kriti <kritijulia@gmail.com>
2023-10-18 02:19:04 +0530

    tree recursion

---
+++ DSA_main/recursion/tree_recursion.cpp
@@ -0,0 +1,14 @@
+// tree recursion - function call itself more than one time
+#include<iostream>
+using namespace std;
+void fun(int n){
+    if(n>0){
+        cout<<n<<" ";
+        fun(n-1);
+        fun(n-1);
+    }
+}
+int main(){
+    fun(3);
+    return 0;
+}
