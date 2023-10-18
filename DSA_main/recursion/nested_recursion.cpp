c12c11c1b5be70c2deea23b03dbef5ea00274cee
kriti <kritijulia@gmail.com>
2023-10-18 20:38:35 +0530

    nested recursion

---
+++ DSA_main/recursion/nested_recursion.cpp
@@ -0,0 +1,14 @@
+// nested recursion
+#include<iostream>
+using namespace std;
+int fun(int n){
+    if(n>100)
+        return n-10;
+    return fun(fun(n+11));
+}
+int main(){
+    int r;
+    r =fun(95);
+    cout<<r;
+    return 0;
+}
